afd6d0a49ebebfb1322ff3c22a06758f88020789
Galia Almaraz <galia.almaraz@uab.edu.bo>
2025-06-05 15:20:50 -0400

    test de division y multiplicacion

--- src/calculator.py
+++ src/calculator.py
@@ -3,3 +3,9 @@ def suma(a, b):
 
 def resta(a, b):
     return a - b
+
+def multiplicacion(a,b):
+    return a * b
+
+def division(a,b):
+    return a / b
--- test/test_calculator.py
+++ test/test_calculator.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.calculator import suma, resta
+from src.calculator import suma, resta, multiplicacion, division
 
 class CalculatorTest(unittest.TestCase):
    
@@ -9,3 +9,9 @@ class CalculatorTest(unittest.TestCase):
 
     def test_resta(self):
         self.assertEqual(resta(10, 5), 5)
+    
+    def test_multiplicacion(self):
+        self.assertEqual(multiplicacion(10, 5), 50)
+        
+    def test_division(self):
+        self.assertEqual(division(8, 2), 4)
